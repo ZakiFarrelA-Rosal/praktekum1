654e6e802602cde4131b97908e8c24fe7644c0db
ZakiFarrelA-Rosal <zadinerosal.1610@gmail.com>
2025-03-07 11:08:51 +0700

    initialcodes

--- praktekum1/app/Http/Controllers/ProdukController.php
+++ praktekum1/app/Http/Controllers/ProdukController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProdukController extends Controller
 {
-    //
+    public function index()
+    {
+        $produk = [
+            ['id' => 1, 'nama' => 'Laptop', 'harga' => 10000000],
+            ['id' => 2, 'nama' => 'Mouse', 'harga' => 250000],
+            ['id' => 3, 'nama' => 'Keyboard', 'harga' => 500000],
+        ];
+
+        return view('produk.index', compact('produk'));
+    }
 }
--- praktekum1/app/Models/Produk.php
+++ praktekum1/app/Models/Produk.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Produk extends Model
+class Produk
 {
-    //
+    public static function all()
+    {
+        return [
+            ['id' => 1, 'nama' => 'Laptop', 'harga' => 10000000],
+            ['id' => 2, 'nama' => 'Mouse', 'harga' => 250000],
+            ['id' => 3, 'nama' => 'Keyboard', 'harga' => 500000],
+        ];
+    }
 }
